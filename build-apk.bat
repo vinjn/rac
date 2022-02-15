0882b5cf739d63bfe9432030a82e4a4d0ab5c6bf
vinjn <vinjn.z@gmail.com>
2022-02-15 13:26:40 +0800

    update build-apk.bat && build-exe.bat

--- build-apk.bat
+++ build-apk.bat
@@ -3,6 +3,7 @@ set PROJECT_FILE=%~dp0\rac.uproject
 
 %RunUAT% BuildCookRun -project=%PROJECT_FILE% -noP4 -ue4exe=UE4Editor-Cmd.exe -compressed -SkipCookingEditorContent^
 -platform=android -targetplatform=android -cookflavor=ASTC ^
--clientconfig=Development -serverconfig=Development -allmaps ^
--cook -build -stage -pak -logtimes -utf8output ^
--archive -archivedirectory=%~dp0\AutoBuild\
+-clientconfig=Development -serverconfig=Development ^
+-cook -iterate -build -stage -pak -utf8output ^
+-logtimes ^
+-archive -archivedirectory=%~dp0 > build-apk.log
